c5743a694351801a11a7e555070e3b2d3e2b26e6
noorulhudaabdulrehman-coder <noorulhudaabdulrehman@gmail.com>
2026-03-15 19:38:04 +0500

    Fix product ID from 4 to 3 in admin.js

--- admin.js
+++ admin.js
@@ -42,7 +42,7 @@ let products = JSON.parse(localStorage.getItem('adminProducts')) || [
     inStock: true 
   },
   { 
-    id: 4, 
+    id: 3, 
     name: "Structured Italian Leather Handbag", 
     reviews: 567, 
     image: "https://images.unsplash.com/photo-1584917865441-4c2b2b8f7b3c?w=600", 
